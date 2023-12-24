b31e09eddeca593f6847dd4f1202acdd7630f8d7
kaitokosuge <kaitokosuge.mi@gmail.com>
2023-12-24 14:35:58 +0900

    [add]login後の画面をtopに変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/top';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
---
+++ resources/js/Pages/Container/TopContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TopContainer() {
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
