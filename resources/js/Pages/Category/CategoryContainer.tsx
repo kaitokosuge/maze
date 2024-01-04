62f7d0b6fc07d9597748b65ae28b4b04cd839ae0
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-01-04 20:29:59 +0900

    [add]クイズの正解、不正解ごとにUIを切り替える実装完了

--- resources/js/Pages/Category/CategoryContainer.tsx
+++ resources/js/Pages/Category/CategoryContainer.tsx
@@ -26,19 +26,19 @@ export default function CategoryContainer( props: any ) {
                 { quizzes.map((quiz: any , index: number) => (
                   <div className='px-5 py-[30px] bg-[#001E41] rounded-[20px] mt-5 flex items-center justify-between'>
                     <p className='font-bold text-[16px] text-limit'>{ index + 1 } { quiz.quiz }</p>
-                    <data className='font-bold text-[12px]'>{ quiz.created_at.slice(0,-17) }</data>
-                    <p className='font-bold text-[12px]'>{ quiz.user.name }</p>
-                    {/* <div>
-                      {quiz.}
-                    </div> */}
+                    <data className='font-bold text-[12px] text-gray-500'>{ quiz.created_at.slice(0,-17) }</data>
+                    <p className='font-bold text-[12px] text-gray-500'>{ quiz.user.name }</p>
+                    <div>
+                        {quiz.is_user_true.some(ob => ob.id === 2) === true ? <p className='font-bold text-[#00FFA3]'>done!!!</p> : <p className='font-bold text-[#1b5841]'>not done</p>}
+                    </div>
                     <p className='rounded-[10px] bg-[#002E64] w-[100px] text-center px-5 py-[7px] font-bold cursor-pointer'>open</p>
-                    {/* <div>
-                      { quiz.choices.map((choice: any,index: number) => (
+                    <div>
+                      {/* { quiz.choices.map((choice: any,index: number) => (
                         <div className=''>
                           <p key={ choice.id }>{ index + 1 }{ choice.choice }</p>
                         </div>
-                      )) }
-                    </div> */}
+                      )) } */}
+                    </div>
                   </div>
                 )) }
               </div>
