a3d657fff07dd5b3d71d3dc05fb7bc109f65322c
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-02-18 13:24:37 +0900

    [add]コメント機能バックエンドファイルセット完了

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function store()
+    {
+        dd("store");
+
+    }
+    public function delete()
+    {
+        dd("delete");
+    }
+}
