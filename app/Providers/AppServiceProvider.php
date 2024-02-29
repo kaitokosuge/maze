1f1c9946916a259d0efaa6ccc0f96f1b4ce5b984
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-02-29 19:25:26 +0900

    [add]https有効化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \URL::forceScheme('https');
+        $this->app['request']->server->set('HTTPS','on');
     }
 }
