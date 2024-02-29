222561b44e039309989fb69554d3d526183e01e6
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-02-29 21:54:13 +0900

    [fix]quiz seeder追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             CategorySeeder::class,
+            QuizSeeder::class,
         ]);
     }
 }
