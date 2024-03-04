d06a354e76d99390d3877126eb58ba7524750b41
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-03-04 16:35:34 +0900

    [fix]quizzesテーブルのquizカラムをlongTextに変更、showDayカラムのnullableを解除

---
+++ database/migrations/2024_03_04_162929_change_quiz_column_type_in_quizzes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('quizzes', function (Blueprint $table) {
+            $table->longText('quiz')->change();
+            $table->string('showDay')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('quizzes', function (Blueprint $table) {
+            $table->string('quiz')->change();
+            $table->string('showDay')->nullable()->change();
+        });
+    }
+};
